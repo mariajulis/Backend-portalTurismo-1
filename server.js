12037ffb0ac032394241ec3a6b37237f8238e740
mariajulis <1067648@senacminas.edu.br>
2025-08-01 14:42:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const contactRoutes = require('./routes/contactRoutes');
 const authRoutes = require('./routes/authRoutes');
 const app = express();
 
-app.use(cors());  
+app.use(cors({                            
+  origin: 'https://frontend-portal-turismo-nine-beta.vercel.app/', // utilizar o link da vercel de vocês        
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true
+}));
 
 // Middleware para parsear JSON no corpo da requisição
 app.use(express.json());
